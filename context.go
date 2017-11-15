9d148cd955a0cc45039ca2cb04128371257580df
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-15 23:23:45 +0900

    refactor: remove ignore methods

--- context.go
+++ context.go
@@ -6,23 +6,7 @@ import (
 )
 
 type Context struct {
-	ctx context.Context
-}
-
-func (c *Context) Deadline() (deadline time.Time, ok bool) {
-	return c.ctx.Deadline()
-}
-
-func (c *Context) Done() <-chan struct{} {
-	return c.ctx.Done()
-}
-
-func (c *Context) Err() error {
-	return c.ctx.Err()
-}
-
-func (c *Context) Value(key interface{}) interface{} {
-	return c.ctx.Value(key)
+	context.Context
 }
 
 func (c *Context) ReportSuccess(reqType, name string, respTime time.Duration, respLength int64) {
